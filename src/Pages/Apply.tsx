2860375b4f63d7f456badfb27a2b9699d23c6e50
Francis <francis.i.authority@gmail.com>
2024-10-17 22:19:29 +0100

    overflow removed.

--- src/Pages/Apply.tsx
+++ src/Pages/Apply.tsx
@@ -3,7 +3,9 @@ const Apply = () => {
   return (
     <main className="page-style">
       <div className="flex flex-col items-center">
-        
+        <div>
+          {/* <img src="/sm alt="" />  */}
+        </div>
       </div>
     </main>
   )
