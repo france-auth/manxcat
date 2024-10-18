40e442812cf77b43b6cca1dd62394b6d6fe27d99
Francis <francis.i.authority@gmail.com>
2024-10-18 12:44:54 +0100

    apply page done.

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -6,7 +6,7 @@ interface ButtonProps {
 }
 const Button: React.FC<ButtonProps> = ({name, onClick, className, link}) => {
   return (
-    <div className={`flex items-center justify-center border-[3px] border-solid border-[#000807] rounded-full ${className} bg-[#EFD0CA] w-60`}>
+    <div className={`flex items-center justify-center border-[3px] border-solid border-[#000807] cursor-pointer rounded-full ${className} bg-[#EFD0CA] w-60`}>
       <a href={link}>
         <button type="button" onClick={onClick} className="text-sm font-bold w-full px-10 py-2">
           {name} 
