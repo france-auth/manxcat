aa88f70a71d62c46429edab66be7fecfc12d9153
Francis <francis.i.authority@gmail.com>
2024-10-20 02:23:58 +0100

    almost done with wallet page.

---
+++ src/Pages/SwapCoin.tsx
@@ -0,0 +1,21 @@
+const SwapCoin = () => {
+  return (
+    <main className="apply_page-style">
+      <div className="flex flex-col items-center">
+        <div className="px-5">
+          <h1 className="text-[27px] font-extrabold">
+            SWAP GOLD COIN
+          </h1>
+          <p className="text-sm">
+            Purchase Gold Coin with USDT
+          </p>
+        </div>
+        <div className="flex justify-between cards px-5">
+
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default SwapCoin
