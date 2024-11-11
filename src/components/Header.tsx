abedb233e26de38c847bbf5cb81348d93e10efab
Francis <francis.i.authority@gmail.com>
2024-11-11 11:52:44 +0100

    homepage adjusted

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,10 +9,11 @@ export default function Header() {
   return (
     <Box
       display={"flex"}
-      gap={2}
       justifyContent={"space-between"}
       alignItems={"center"}
-      width={"85%"}
+      width={"full"}
+      paddingX={"20px"}
+      paddingTop={"16px"}
     >
       <Box
         display={"flex"}
@@ -23,33 +24,32 @@ export default function Header() {
         borderRadius={"50%"}
         p={"10px"}
         w={"80px"}
+        className="cursor-pointer"
       >
         <Link to={"/level"} className="w-[100%]">
           <Image src="/sm-cat.png" />
         </Link>
       </Box>
-      <Flex gap={2}>
+      <Flex display={"flex"} flexDirection={"column"} justifyContent={"center"} alignItems={"end"} gap={2}>
         <Flex
           alignItems={"center"}
           gap={2}
-          h={"40px"}
           bg={"#EFD0CA"}
           borderRadius={"25px"}
-          p={"0px 10px"}
+          p={"10px 12px"}
         >
           <Image src="/coin.png" w={"15px"} />
-          <Text fontSize={"12px"} color={"#000807"}>
+          <Text fontSize={"13px"} color={"#000807"}>
             {coinsEarned.toFixed(3)}
           </Text>
           <Icon as={FaPlus} w={"15px"} color={"#000807"} />
         </Flex>
-        {/* <Flex alignItems={'center'} gap={2} h={'40px'} bg={'#EFD0CA'} borderRadius={'25px'} p={'0px 15px'}>
-                <Image src="/coin.png" w={'15px'}/>
-                <Text fontSize={'12px'} color={"#000807"}>
-                    45,000
-                </Text>
-                <Icon as={FaPlus} w={'15px'} color={'#000807'}/>
-            </Flex> */}
+        <Flex alignItems={'center'} gap={2} h={'40px'} bg={'#EFD0CA'} borderRadius={'25px'} p={'0px 15px'}>
+          <Image src="/levelcoin-homepage.png" w={'15px'}/>
+          <Text fontSize={'12px'} color={"#000807"}>
+              100
+          </Text>
+        </Flex>
       </Flex>
     </Box>
   );
