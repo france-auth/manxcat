46c9e88e969171c34fa113356dd934c41249d8d9
Levi <70198955+Draysongz@users.noreply.github.com>
2024-11-13 16:37:32 +0100

    done

--- src/components/IdModal.tsx
+++ src/components/IdModal.tsx
@@ -19,24 +19,27 @@ import React from 'react'
 import { CloseIcon, Icon } from '@chakra-ui/icons';
 
 const IdModal: React.FC<IdModalProps> = ({isOpen, isClosed}) => {
-    return (!isOpen() ? null : (
-        <div
-            className='fixed inset-0 bg-[#EFD0CA] bg-opacity-40 backdrop-brightness-50 flex justify-center items-center z-[999] p-5'
+    return !isOpen() ? null : (
+      <div className="fixed inset-0 bg-[#EFD0CA] bg-opacity-40 backdrop-brightness-50 flex justify-center items-center z-[999] p-5">
+        <div className="flex flex-col bg-[#EFD0CA] rounded-3xl p-4 w-full">
+          <div
+            className="flex justify-end mt-3 mb-1 items-center "
             onClick={() => isClosed()}
-        >
-            <div className='flex flex-col bg-[#EFD0CA] rounded-3xl p-4 w-full'>
-                <div className='flex justify-between mt-3 mb-1 items-center'>
-                    <Icon as={CloseIcon } boxSize={6} />
-                </div>
-                <div className='mx-3 text-[14.5px]'>
-                        <p
-                        className='my-3 flex justify-center'>
-                            Grow your bond with your Soul by petting and chatting, and gifting items.
-                        </p>
-                        <p className='mb-2'>As you build affection, you'll earn more points. Who knows? Higher affection might bring some nice surprises your way!</p>
-                </div>
-            </div>
+          >
+            <Icon as={CloseIcon} boxSize={6} />
+          </div>
+          <div className="mx-3 text-[14.5px]">
+            <p className="my-3 flex justify-center">
+              Grow your bond with your Soul by petting and chatting, and gifting
+              items.
+            </p>
+            <p className="mb-2">
+              As you build affection, you'll earn more points. Who knows? Higher
+              affection might bring some nice surprises your way!
+            </p>
+          </div>
         </div>
-    ))
+      </div>
+    );
 }
 export default IdModal
